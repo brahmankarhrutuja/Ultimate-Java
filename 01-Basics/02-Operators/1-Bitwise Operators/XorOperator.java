04b407ee39b6d034b8c7b068742c0ba5d78765b2
Hrutuja Rahul Brahmankar <141763509+brahmankarhrutuja@users.noreply.github.com>
2024-07-02 13:53:24 +0000

    made class of every operator

--- 01-Basics/02-Operators/1-Bitwise Operators/XorOperator.java
+++ 01-Basics/02-Operators/1-Bitwise Operators/XorOperator.java
@@ -2,6 +2,9 @@ class XorOperator
 {
     public static void main(String[] args)
     {
-        
+        var a = 5;
+        var b = 3;
+        var result = a^b;
+        System.out.println(result);
     }
 }
