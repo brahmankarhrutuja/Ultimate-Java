2b5337129a5c19d43414c2acf6d9d2683726e9e2
Hrutuja Rahul Brahmankar <141763509+brahmankarhrutuja@users.noreply.github.com>
2024-07-02 14:11:15 +0000

    Added more Operators

---
+++ 01-Basics/02-Operators/1-Bitwise Operators/LeftShift.java
@@ -0,0 +1,9 @@
+class LeftShift
+{
+    public static void main(String[] args)
+    {
+        var a = 2;
+        var result = a << 2;
+        System.out.println(result);
+    }
+}
---
+++ 01-Basics/02-Operators/1-Bitwise Operators/RightShift.java
@@ -0,0 +1,9 @@
+class RightShift
+{
+    public static void main(String[] args)
+    {
+        var a = 1;
+        var result = a<<3;
+        System.out.println(result);
+    }
+}
