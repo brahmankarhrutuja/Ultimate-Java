86dfdc7c0e476a22f9924d2ebb649c25fd6d5030
Hrutuja Rahul Brahmankar <141763509+brahmankarhrutuja@users.noreply.github.com>
2024-07-04 03:37:05 +0000

    added one more file of same program but different method

---
+++ 01-Basics/07-Conditional Control Constructs/02-If Else/ElseIfLadder2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class Marks
+{
+    public static void main(String [] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Your Marks:");
+        int marks= scan.nextInt();       
+        printGrade(marks);
+    }
+    public static void printGrade(int marks)
+    {    
+        if (marks>90 && marks<=100) 
+        {
+            System.out.println("Grade A");
+        }
+        else if (marks>70 && marks<=90)
+        {
+            System.out.println("Grade B");
+        }
+        else if (marks>50 && marks<=70)
+        {
+            System.out.println("Grade C");
+        }
+        else if (marks>=35 && marks<=50)
+        {
+            System.out.println("Grade D");
+        }
+        else if (marks>=0 && marks<35)
+        {
+            System.out.println("oopppsss Fail");
+        }
+        else
+        {
+            System.out.println("Phissss! Invalid Marks"); 
+        }
+    }
+}
