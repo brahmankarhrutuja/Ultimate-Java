0b8b0e8fbfc741b3b4b03f386ff59511e4721ab2
Hrutuja Rahul Brahmankar <141763509+brahmankarhrutuja@users.noreply.github.com>
2025-01-13 12:20:33 +0000

    Add Reverse file

---
+++ 00- FrequentlyAskedQuestion/3- Reverse the Number/ReverseNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+class Reversenumber{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scan.nextInt();
+        int rev = 0;
+        while (num != 0)
+        {
+            rev = rev*10 + num%10;
+            num = num/10;
+        }
+        System.out.println("Reverse num is: " +rev);
+    }
+}
